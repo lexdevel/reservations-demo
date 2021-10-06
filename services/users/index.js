24ca96bd1ea0145050651c97017fe2abb43e91fa
Alexander Logvinenko <lexdevel@gmail.com>
2021-10-06 19:09:11 +0300

    Structure refactoring.

--- services/users/index.js
+++ services/users/index.js
@@ -1,13 +1,15 @@
 import { buildFederatedSchema } from '@apollo/federation';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server';;
+import { Mutex } from 'async-mutex';
 import { readFileSync } from 'fs';
 
 const users = JSON.parse(readFileSync('./users.json').toString('utf-8'));
+const lock = new Mutex();
 const typeDefs = gql(readFileSync('./schema.graphql').toString('utf-8'));
 
 const resolvers = {
   Query: {
-    users: () => users,
+    users: (parent, args, context, info) => users,
   },
   User: {
     __resolveReference: reference => users.find(user => user.id === reference.id),
